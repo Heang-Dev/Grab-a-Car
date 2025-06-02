aac00d20a32c07adf3b3e91860a773944578a8a8
Chhun Mengheang <chhunmengheang5@gmail.com>
2025-06-02 09:53:12 +0700

    Route parameters regex

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ Route::get('/', function () {
 
 Route::view('/about', 'about');
 
-Route::get('/products/{id?}', function (string $id) {
-    return 'Work! : ' . $id;
-})->whereNumber('id');
+Route::get('/user/{username}', function (string $username) {
+    return 'username : ' . $username;
+})->where('username', '[a-z]+');
+
+Route::get('{lang}/product/{id}', function (string $lang, string $id) {
+    return 'lang : ' . $lang . 'id : ' . $id;
+})->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}']);
+
+Route::get('/search/{search}', function (string $search) {
+    return 'search : ' . $search;
+})->where('search', '.+');
