b73a5e6eb14501f40ddd5b199aa74dfa40af485a
Chhun Mengheang <chhunmengheang5@gmail.com>
2025-06-02 09:19:56 +0700

    Setup project and practices with route required parameters

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    $person = [
+        "name" => "Jack",
+        "email" => "jack@gmail.com",
+    ];
+    dd($person);
+    return view('welcome');
+});
+
+Route::view('/about', 'about');
+
+Route::get('/products/{id}', function (string $id) {
+    return 'Products ID : ' . $id;
+});
