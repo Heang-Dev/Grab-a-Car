b7831e3a8452887d11c77394e1f39eaf650deb78
Chhun Mengheang <chhunmengheang5@gmail.com>
2025-06-02 10:01:29 +0700

    Route named

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $person = [
-        "name" => "Jack",
-        "email" => "jack@gmail.com",
-    ];
-    dd($person);
+    // $aboutURL = url('/about');
+    $aboutPageURL = route('about'); // Route name automatically updated when route is changed
+    dd('About Page URL: ' . $aboutPageURL);
     return view('welcome');
 });
 
-Route::view('/about', 'about');
-
-Route::get('/user/{username}', function (string $username) {
-    return 'username : ' . $username;
-})->where('username', '[a-z]+');
-
-Route::get('{lang}/product/{id}', function (string $lang, string $id) {
-    return 'lang : ' . $lang . 'id : ' . $id;
-})->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}']);
-
-Route::get('/search/{search}', function (string $search) {
-    return 'search : ' . $search;
-})->where('search', '.+');
+Route::view('/about-us', 'about')->name('about');
