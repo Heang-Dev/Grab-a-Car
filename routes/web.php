fc1fd5530629c4212eebf24126825c44d17e4e60
Chhun Mengheang <chhunmengheang5@gmail.com>
2025-06-02 10:13:21 +0700

    Name routes with parameters

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // $aboutURL = url('/about');
-    $aboutPageURL = route('about'); // Route name automatically updated when route is changed
-    dd('About Page URL: ' . $aboutPageURL);
+    
+    $productURL = route('product.view', ['lang' => 'en', 'id' => 1]);
+    dd($productURL);
+    
     return view('welcome');
 });
 
-Route::view('/about-us', 'about')->name('about');
+Route::get('/{lang}/product/{id}', function (string $lang, int $id) {
+    // if route url changes (ex: '/add/{lang}/test/product/{id}'),
+    // the route is stealed working
+})->name('product.view');
