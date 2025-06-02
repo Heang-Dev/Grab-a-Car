b214ae43b9cfb2a84d775f206a8335e80a58b848
Chhun Mengheang <chhunmengheang5@gmail.com>
2025-06-02 10:58:11 +0700

    Basic Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,4 @@ Route::get('/about', function(){
     return 'About';
 })->name('about');
 
-Route::fallback(function () {
-    return '404';
-});
+Route::get('/car', [CarController::class,'index']);
